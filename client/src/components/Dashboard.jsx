bbae8c7c5776878b173bfd0f8db08b7f8c3079e4
23cssahil <23cssahil@gail.com>
2026-07-19 09:17:04 +0530

    Adjust timer padding to be perfectly below the globe

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -738,7 +738,7 @@ export default function Dashboard() {
                 position: 'absolute', top: 0, left: 0, width: '100%', height: '100%',
                 display: 'flex', flexDirection: 'column', alignItems: 'center', pointerEvents: 'none',
                 zIndex: 5,
-                paddingTop: '75vh'
+                paddingTop: '65vh'
               }}
             >
               {isSearchingRandom && (
